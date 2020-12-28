9b2b46fe56ba639ce36ea6a85d0f7fdfa7093691
Nick Nesler <nicknesler@gmail.com>
2020-12-28 14:36:30 -0500

    updte documentation on vbResourceRouter

--- routes/vbResourcesRouter.js
+++ routes/vbResourcesRouter.js
@@ -4,8 +4,8 @@ const Responder = require('../middleware/responder');
 
 vbResourcesRouter.route('/')
 /**
- * @api {get} /api/ebayresources?eid={eid} Get ebay resources for eid
- * @apiParam {String} eid of ebay item
+ * @api {get} /api/vbresources?path={path} Get meta information for vintagebreaks.com/{path}
+ * @apiParam {String} path of item on vintage breaks
  */
 .get(vbResourcesController.getVbItem, Responder);
 
